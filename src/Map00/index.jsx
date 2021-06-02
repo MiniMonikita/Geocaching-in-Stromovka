6f09dc35f1a427afffad003dafe0f55ceb05537d
Monika Kalajtzidu <m.kalajtzidu@gmail.com>
2021-06-02 20:55:55 +0200

    geolocation control

--- src/Map00/index.jsx
+++ src/Map00/index.jsx
@@ -14,29 +14,67 @@ import "./zoom.css";
 const Map00 = () => {
   const pinpoints = [
     {
+      // PLANETARIUM - GIORDANO BRUNO
       id: 1,
       ikonaUrl: infoUrl,
-      latitude: 50.1078692,
-      longitude: 14.4241589,
+      latitude: 50.105291,
+      longitude: 14.427648,
     },
     {
+      // RYBNIK - DUCK HEADS alebo ROSTLINKY TABULA
       id: 2,
       ikonaUrl: infoUrl,
-      latitude: 50.10766,
-      longitude: 14.4175392,
+      latitude: 50.1070722,
+      longitude: 14.4221119,
     },
     {
+      // CITARNA
       id: 3,
       ikonaUrl: infoUrl,
-      latitude: 50.1049017,
-      longitude: 14.4141086,
+      latitude: 50.1076472,
+      longitude: 14.4176525,
+    },
+    {
+      // STARA FONTANA + JAPANESE TREE
+      id: 4,
+      ikonaUrl: infoUrl,
+      latitude: 50.1058994,
+      longitude: 14.4140292,
+    },
+    {
+      // STUDNICNI DOMEK
+      id: 5,
+      ikonaUrl: infoUrl,
+      latitude: 50.1049194,
+      longitude: 14.4083556,
+    },
+    {
+      // PAMATNIK MECSEROVY SILNICE
+      id: 6,
+      ikonaUrl: infoUrl,
+      latitude: 50.1044306,
+      longitude: 14.4150547,
+    },
+    {
+      // OSTROVCEK - JEZERO SLUNECNICE
+      id: 7,
+      ikonaUrl: infoUrl,
+      latitude: 50.1062383,
+      longitude: 14.4166774,
+    },
+    {
+      // SLECHTOVA RESTAURACE - 30 HLAV
+      id: 8,
+      ikonaUrl: infoUrl,
+      latitude: 50.1048456,
+      longitude: 14.417837,
     },
   ];
 
   const [viewport, setViewport] = useState({
     latitude: 50.1045369,
     longitude: 14.4310347,
-    zoom: 15,
+    zoom: 13,
   });
 
   const [popupOtevren, setPopupOtevren] = useState(false);
@@ -74,7 +112,19 @@ const Map00 = () => {
       >
         <div className="ovladani">
           <NavigationControl />
-          <GeolocateControl />
+          <GeolocateControl
+            style={{
+              right: 10,
+              top: 10,
+            }}
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+            showAccuracyCircle={false}
+            onGeolocate={(event) => {
+              console.log(event);
+            }}
+            auto
+          />
         </div>
 
         <Marker
